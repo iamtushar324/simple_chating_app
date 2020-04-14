68473fcf0d1e1813a71bc43598ff934b0946be88
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-04-14 14:21:44 +0530

    added login feature
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

--- public/app.js
+++ public/app.js
@@ -1,1 +1,72 @@
-io()
+
+
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *client
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+
+// postData('/login', { username: "tusa", password: "ppppp" })
+//     .then((data) => {
+//         console.log(data);
+//     });
+
+
+document.getElementById("login_btn").onclick = () => {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    postData('/login', { username: username, password: password })
+        .then((data) => {
+
+            if (data.username == username) {
+
+                document.getElementById('loign').style.display = "none"
+                document.getElementById('head').innerHTML = `Welcome , ${username}`
+                document.getElementById('chatRoom').style.display = "block"
+
+                let socket = io()
+
+                socket.on("msg_re", () => {
+
+                    let temp = document.createElement('li').innerHTML(msg.msg)
+
+                    document.getElementById("msg_list").appendChild(temp)
+
+
+                })
+
+
+
+                document.getElementById("send_btn").onclick = () => {
+
+                    let sendTo = document.getElementById("sendTo").value
+                    let msg = document.getElementById("msg").value
+
+
+
+                    socket.emit("msg_send", { msg: msg, to: sendTo })
+                }
+            }
+
+
+        }
+
+
+
+
+        });
+}
+
--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const { db, chat_users } = require('./db/db')
 const server = http.createServer(app)
 const io = socketio(server)
 
+app.use(exp.urlencoded({ extended: false }));
+app.use(exp.json());
+
 app.use(exp.static(__dirname + "/public"))
 
 io.on('connection', (socket) => {
@@ -18,10 +21,59 @@ io.on('connection', (socket) => {
 
 
 
+app.post('/login', async (req, res) => {
 
-
-db.sync(() => {
-    server.listen(2020, () => {
-        console.log("server started At http://localhost:2000")
+    const currentUser = await chat_users.findOne({
+        where: {
+            username: req.body.username
+        }
     })
+
+    if (currentUser) {
+        if (currentUser.password == req.body.password) {
+            res.send({ username: currentUser.username })
+        }
+
+        else {
+            res.send({ error: "password is wrong" })
+        }
+    }
+
+    else {
+        const newUser = await chat_users.create({
+            username: req.body.username,
+            password: req.body.password
+        })
+            .then((u) => {
+                res.send({ username: u.username })
+            })
+            .catch(() => {
+                res.send({
+                    error: "May be user aleredy exist , try different username"
+                })
+            })
+    }
+
+
+})
+
+io.on("msg_send", (socket) => {
+
+    io.to(socket.to).emit("msg_re", {
+        msg: socket.msg
+    })
+
+
+})
+
+
+
+
+
+db.sync().then(() => {
+
+    server.listen(2020, () => {
+        console.log("server started At http://localhost:2020")
+    })
+
 })
